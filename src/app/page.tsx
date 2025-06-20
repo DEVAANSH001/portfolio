27d67f323bc6cb8b3a090562e312817b96f313c1
DEVAANSH001 <theman3185@gmail.com>
2025-06-20 23:17:31 +0530

    footer done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
 import Hero from "@/components/hero"
-// import Experience from "@/components/experience"
-// import Skills from "@/components/skills"
-// import Projects from "@/components/projects"
-// import Contact from "@/components/contact"
+import Experience from "@/components/Experience"
+import Skills from "@/components/skills"
+import Projects from "@/components/projects"
+import Contact from "@/components/contact"
+import Footer from "@/components/Footer"
 
 export default function Home() {
   return (
     <div className="mx-auto max-w-5xl space-y-20 py-10">
       <Hero />
-      {/* <Experience />
-      <Education />
+      <Experience />
       <Skills />
-      <Projects />
-      <Contact /> */}
+       <Projects/>
+       <Contact />
+       <Footer />
     </div>
   )
 }
