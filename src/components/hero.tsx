593a19cc91c118e8ac57f98ae041f1a7d55be941
DEVAANSH001 <theman3185@gmail.com>
2026-02-01 01:42:33 +0530

    updated resume

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -73,7 +73,7 @@ export default function Hero() {
           </p>
 
           <div className="flex flex-wrap gap-2 sm:gap-3">
-            <Link href="/Devaansh.pdf" target="_blank" download>
+            <Link href="/RESUME.pdf" target="_blank" download>
               <Button
                 size="sm"
                 className="text-xs sm:text-sm bg-blue-600 hover:bg-purple-600 text-white rounded-full px-4 sm:px-5 py-2 transition-colors duration-500"
